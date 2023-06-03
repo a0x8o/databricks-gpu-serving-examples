6143b34a59f159ef7dbd13294e3c1904f08a1dfc
ahdbilal <ahdbilal>
2023-06-03 10:30:33 +0000

    update dolly-v2-12b

--- examples/dolly-v2-12b(Quantized).py
+++ examples/dolly-v2-12b(Quantized).py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 !pip install --upgrade torch
-!pip install accelerate
+!pip install --upgrade accelerate
 !pip install bitsandbytes
 !pip install pynvml
 import pandas as pd
@@ -11,30 +11,31 @@ import torch
 
 # COMMAND ----------
 
-from pynvml import *
+# Load the model and quantize it
+quantization_config = transformers.BitsAndBytesConfig(
+    load_in_8bit=True, bnb_8bit_compute_dtype=torch.bfloat16
+)
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    "databricks/dolly-v2-12b", padding_side="left"
+)
+quantized_model = transformers.AutoModelForCausalLM.from_pretrained(
+    "databricks/dolly-v2-12b",
+    quantization_config=quantization_config,
+    device_map="auto",
+)
 
-def print_gpu_utilization():
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU memory occupied: {info.used//1024**2} MB.")
-
-
-def print_summary(result):
-    print(f"Time: {result.metrics['train_runtime']:.2f}")
-    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
-    print_gpu_utilization()
-
-print_gpu_utilization()
-
-# COMMAND ----------
-
-from huggingface_hub import snapshot_download
-# Download the Dolly model snapshot from huggingface
-snapshot_location = snapshot_download(repo_id="databricks/dolly-v2-12b")
+# Save the quantized weights and tokenizer to a temp location - we would package these with MLflow model
+snapshot_location = "/root/.cache/quantized_model"
+tokenizer.save_pretrained(snapshot_location)
+quantized_model.save_pretrained(snapshot_location)
 
 # COMMAND ----------
 
+
+
+# COMMAND ----------
+
+# Create a MLflow custom pyfunc class
 class Dolly(mlflow.pyfunc.PythonModel):
     def load_context(self, context):
         """
@@ -42,16 +43,14 @@ class Dolly(mlflow.pyfunc.PythonModel):
         using the specified model repository.
         """
         # Initialize tokenizer and language model
-
-        quantization_config = BitsAndBytesConfig(load_in_8bit=True, bnb_8bit_compute_dtype=torch.bfloat16)
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-            context.artifacts['repository'], 
-            padding_side="left")
+            context.artifacts["repository"], padding_side="left"
+        )
         self.model = transformers.AutoModelForCausalLM.from_pretrained(
-            context.artifacts['repository'], 
-            quantization_config=quantization_config,
+            context.artifacts["repository"],
             device_map="auto",
-            pad_token_id=self.tokenizer.eos_token_id)
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
         self.model.eval()
 
     def _build_prompt(self, instruction):
@@ -83,17 +82,24 @@ class Dolly(mlflow.pyfunc.PythonModel):
         prompt = self._build_prompt(message)
 
         # Encode the input and generate prediction
-        encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_length=max_tokens)
-    
+        encoded_input = self.tokenizer.encode(prompt, return_tensors="pt").to("cuda")
+        output = self.model.generate(
+            encoded_input,
+            do_sample=True,
+            temperature=temperature,
+            max_length=max_tokens,
+        )
+
         # Decode the prediction to text
         generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
 
         # Removing the prompt from the generated text
-        prompt_length = len(self.tokenizer.encode(prompt, return_tensors='pt')[0])
-        generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
+        prompt_length = len(self.tokenizer.encode(prompt, return_tensors="pt")[0])
+        generated_response = self.tokenizer.decode(
+            output[0][prompt_length:], skip_special_tokens=True
+        )
 
-        return generated_response
+        return generated_response  #
 
 # COMMAND ----------
 
@@ -101,50 +107,54 @@ from mlflow.models.signature import ModelSignature
 from mlflow.types import DataType, Schema, ColSpec
 
 # Define input and output schema
-input_schema = Schema([
-    ColSpec(DataType.string, "message"), 
-    ColSpec(DataType.double, "temperature"), 
-    ColSpec(DataType.long, "max_tokens")])
+input_schema = Schema(
+    [
+        ColSpec(DataType.string, "message"),
+        ColSpec(DataType.double, "temperature"),
+        ColSpec(DataType.long, "max_tokens"),
+    ]
+)
 output_schema = Schema([ColSpec(DataType.string)])
 signature = ModelSignature(inputs=input_schema, outputs=output_schema)
 
 # Define input example
-input_example=pd.DataFrame({
-            "message":["what is ML?"], 
-            "temperature": [0.5],
-            "max_tokens": [100]})
+input_example = pd.DataFrame(
+    {"message": ["what is ML?"], "temperature": [0.8], "max_tokens": [250]}
+)
 
 # Log the model with its details such as artifacts, pip requirements and input example
-with mlflow.start_run() as run:  
+with mlflow.start_run() as run:
     mlflow.pyfunc.log_model(
         "model",
         python_model=Dolly(),
-        artifacts={'repository' : snapshot_location},
-        pip_requirements=["torch", "transformers", "accelerate","bitsandbytes"],
+        artifacts={"repository": snapshot_location},
+        pip_requirements=["torch", "transformers", "accelerate", "bitsandbytes"],
         input_example=input_example,
         signature=signature,
     )
 
-
 # COMMAND ----------
 
 # Register model in MLflow Model Registry
-result = mlflow.register_model(
-    "runs:/"+run.info.run_id+"/model",
-    "dolly-v2-12b"
-)
+result = mlflow.register_model("runs:/" + run.info.run_id + "/model", "dolly-v2-12b")
 # Note: Due to the large size of the model, the registration process might take longer than the default maximum wait time of 300 seconds. MLflow could throw an exception indicating that the max wait time has been exceeded. Don't worry if this happens - it's not necessarily an error. Instead, you can confirm the registration status of the model by directly checking the model registry. This exception is merely a time-out notification and does not necessarily imply a failure in the registration process.
 
 # COMMAND ----------
 
 # Load the logged model
+# In case of a GPU memory error, use the following code snippet to clear the memory (Might have to run this multiple times):
+# import gc
+# del loaded_model
+# torch.cuda.empty_cache()
+# gc.collect()
 loaded_model = mlflow.pyfunc.load_model(f"runs:/{run.info.run_id}/model")
-print_gpu_utilization()
 
 # COMMAND ----------
 
 # Make a prediction using the loaded model
-input_example=pd.DataFrame({"message":["what is ML?"], "temperature": [0.5],"max_tokens": [100]})
+input_example = pd.DataFrame(
+    {"message": ["what is ML?"], "temperature": [0.5], "max_tokens": [100]}
+)
 loaded_model.predict(input_example)
 
 # COMMAND ----------
